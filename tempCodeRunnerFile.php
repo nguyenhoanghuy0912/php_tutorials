f494cde26df7d760581d53c4e1c283542c01d213
huy_nh <89738520+nguyenhoanghuy0912@users.noreply.github.com>
2023-06-23 23:00:11 +0700

    Delete tempCodeRunnerFile.php

--- tempCodeRunnerFile.php
+++
@@ -1,8 +0,0 @@
-<?php
-function removeLastCharacter($str) {
-    $lastChar = substr($str, -1);
-    return rtrim($str, $lastChar);
-}
-$str_3 ='abc';
-$trimmedStr = removeLastCharacter($str_3);
-echo $trimmedStr . "\n";
